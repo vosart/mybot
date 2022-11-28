3e1849dd5e1abb31cc13585535cf44a075e05945
Artem Voskresensky <metravs@gmail.com>
2022-11-29 01:27:10 +0300

    добавлен wordcount, next_full_moon

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 import ephem
 import datetime
+import locale
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -18,18 +19,37 @@ def talk_to_me(update, context):
 def greet_user(update, context):
     logger.debug("Вызван /start")
     first_name = update.message['chat']['first_name']
-    last_name = update.message['chat']['last_name'] or ''
-    update.message.reply_text(f"Здравствуй, {first_name} {last_name.strip()}!")   
+    update.message.reply_text(f"Здравствуй, {first_name}!")   
+    update.message.reply_text('Доступные команды: \n/start\n /planet Название планеты (например Mars)\n /wordcount\n /next_full_moon')
 
+# Определение в каком созвездии находится планета Солнечной системы
 def constellation(update, context):
     logger.debug("Вызван /planet")
     date = datetime.datetime.now()
-    user_text = update.message.text.split()
+    user_text = update.message.text
+    if user_text[8:] == '':
+        raise ValueError(update.message.reply_text('Вы не ввели планету!'))
+    user_text = user_text.split()
     planet = user_text[1]
     func = getattr(ephem, planet)
     const = ephem.constellation(func(date))[1]
-    update.message.reply_text(f'{planet} сейчас находится в создвездии {const}')
+    update.message.reply_text('{planet} сейчас находится в создвездии {const}'.format(planet = planet, const = const))
 
+# Счётчик слов в предложении    
+def wordcount(update, context):
+    logger.debug("Вызван /wordcount")
+    user_text = update.message.text.strip()
+    if user_text[11:] == '':
+        raise ValueError(update.message.reply_text('Вы ничего не ввели!'))
+    user_text = user_text[11:].split()
+    update.message.reply_text(f'В предложении {len(user_text)} слов')
+
+# Ближайшее полнолуние
+def next_full_moon(update, context):
+    logger.debug("Вызван /next_full_moon")
+    date = datetime.datetime.now()    
+    nfm_date = ephem.next_full_moon(date)
+    update.message.reply_text(f'Следующее полнолуние {nfm_date}')
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
@@ -37,6 +57,9 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", constellation))
+    dp.add_handler(CommandHandler("wordcount", wordcount))
+    dp.add_handler(CommandHandler("next_full_moon", next_full_moon))
+    
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     logger.info("Бот стартовал")
